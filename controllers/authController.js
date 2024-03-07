84f3f2a8d6575b532ebe6987687988bd37a39020
Chitra Nayal <rekhachitra96@gmail.com>
2024-03-07 08:28:24 +0530

    brain-teaser

---
+++ controllers/authController.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const userData = require('./db/users.js');
+const jwt = require('jsonwebtoken');
+
+const loginHandler = (req,res)=>{
+    const {username, password} = req.body;
+    const isDataValid = userData.data.some((user)=>user.username === username && user.password === password);
+    if(isDataValid){
+        let token = jwt.sign({username},process.env.SECRET_KEY);
+        res.json({username, token,message:`user ${username} logged in successfully`})
+    }
+    else
+        res.json({message:"invalid user"});
+}
+
+module.exports = loginHandler;
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const quiz_data = require("./db/quiz_data");
 const cors = require('cors');
 const quizRouter = require('./routers/quizRouter.js');
-const userData = require('./db/users.js');
-const jwt = require('jsonwebtoken');
+
 require('dotenv').config();
+const loginRouter = require('./routers/loginRouter.js');
+
 
 const app = express();
 app.use(cors());
@@ -17,22 +17,9 @@ app.get('/', (req,res)=>{
     res.send("hi chitra")
 })
 
-// app.get('/quiz',(req,res)=>{
-//     res.send(quiz_data.data);
-// })
-
 app.use('/quiz',quizRouter);
 
-app.post('/login', (req,res)=>{
-    const {username, password} = req.body;
-    const isDataValid = userData.data.some((user)=>user.username === username && user.password === password);
-    if(isDataValid){
-        let token = jwt.sign({username},process.env.SECRET_KEY);
-        res.json({username, token,message:`user ${username} logged in successfully`})
-    }
-    else
-        res.json({message:"invalid user"});
-})
+app.use('/login', loginRouter)
 
 app.listen(process.env.PORT || PORT , ()=>{
     console.log("server started");
