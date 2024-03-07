e1b16290fd6bf9ddf5a104ec428aa3df7d298b13
Chitra Nayal <rekhachitra96@gmail.com>
2024-03-07 08:19:51 +0530

    brain-teaser

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.post('/login', (req,res)=>{
     const {username, password} = req.body;
     const isDataValid = userData.data.some((user)=>user.username === username && user.password === password);
     if(isDataValid){
-        let token = jwt.sign({username},"b94fbd385762615dd3eac7a195f800739dbdc07949673da5ef56f83b6c81265332c6b19a9e59acb399e1357aff9c06f4348e0aaa19908b5fb7d3f5440b668f87cabcaf5d4a9fc19fd2d80664f8311f081c39fb607d640321682c8d8b543c5a2af1549191c1f96a39466f55f946204818162cc47ebaa09c4da64b18306cff04cbf05eb00a1e4581cf6ddd1f457142bbb46e89bc9a6fd15ce915edb8844e6def3dab18fabd753cc94c7ad8e150391bc87c5acd3f1819718e140ea18f5af910e8f111b208e675d3d81c88eed3371c62ad12fa143aa49daf68f419bdb83af92d8716c0df81d4347f13104d8607f0c73473bba7bb873dd34528d118d702dc33e437d0");
+        let token = jwt.sign({username},process.env.SECRET_KEY);
         res.json({username, token,message:`user ${username} logged in successfully`})
     }
     else
